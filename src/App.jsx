f58c38b1390874f2fe1bd057fc170f1aefef1042
julian-hzd <julianhzd@hotmail.com>
2023-11-24 15:15:09 -0500

    intialize project with vite, react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="">
+      <p className="text-red-400">bc</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{html,jsx,js}",
+    "./src/**/*.{html,jsx,js}",
+    "./index.html",
+  ],
+  // ...
+};
