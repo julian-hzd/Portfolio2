9491c648162e0fdf381c4f6385b3c5ed4a1d7a0d
julian-hzd <julianhzd@hotmail.com>
2023-11-30 20:50:11 -0500

    Potential base of client side routing, though nested routes is more appealing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,33 +5,43 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import Projects from "../components/Projects";
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<Home />}>
-//       // <Route path="contact" element={<Contact />} />
-//     </Route>,
-//   ),
-// );
-//
-//
+const nav = (
+  <nav>
+    <ul>
+      <li>
+        <a href="/">~</a>
+      </li>
+      <li>
+        <a href="projects">Projects</a>
+      </li>
+      <li>
+        <a href="About">About</a>
+      </li>
+      <li>
+        <a href="Contact me">Contact me</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+]);
+
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="home">~</a>
-        </li>
-        <li>
-          <a href="projects">Projects</a>
-        </li>
-        <li>
-          <a href="About">About</a>
-        </li>
-        <li>
-          <a href="Contact me">Contact me</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {nav}
+      <RouterProvider router={router}></RouterProvider>
+    </>
   );
 }
