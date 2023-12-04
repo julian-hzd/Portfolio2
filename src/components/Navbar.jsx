89af70aad5fe0379c9d5c15894b264522b6074da
julian-hzd <julianhzd@hotmail.com>
2023-12-04 07:57:38 -0500

    Navbar hover li effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,33 @@
 import { Link, Outlet } from "react-router-dom";
 
 export default function Navbar() {
+  const liHoveredColor = "hover:text-red-200";
   return (
     <>
       <nav className="mt-10">
         <ul
           className="flex flex-wrap justify-center space-x-10 
-          text-2xl text-sky-200 hover:text-red-50"
+          text-2xl text-sky-200"
         >
           <li>
-            <Link to={`/`}>~</Link>
+            <Link to={`/`} className={liHoveredColor}>
+              ~
+            </Link>
           </li>
           <li>
-            <Link to={`projects`}>Projects</Link>
+            <Link to={`projects`} className={liHoveredColor}>
+              Projects
+            </Link>
           </li>
           <li>
-            <Link to={`about`}>About</Link>
+            <Link to={`about`} className={liHoveredColor}>
+              About
+            </Link>
           </li>
           <li>
-            <Link to={`contact`}>Contact me</Link>
+            <Link to={`contact`} className={liHoveredColor}>
+              Contact me
+            </Link>
           </li>
         </ul>
       </nav>
