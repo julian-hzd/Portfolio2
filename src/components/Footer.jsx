72fe14eb1856e4bfadfa461d3d57629cec9421eb
julian-hzd <julianhzd@hotmail.com>
2023-12-06 13:48:43 -0500

    Github and Linkedin footer and urls

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,17 @@ import linkedinLogo from "/icons8-linkedin.svg";
 
 export default function Footer() {
   return (
-    <div
-      className="flex flex-wrap justify-center space-x-10 
-      h-screen sticky bottom-0"
-    >
-      <img src={githubLogo} href="#" />
-      <img src={linkedinLogo} />
-    </div>
+    <footer className="flex flex-wrap justify-center space-x-10 h-4 absolute bottom-40 w-full">
+      <a href="https://github.com/julian-hzd" target="_blank" rel="noreferrer">
+        <img src={githubLogo} />
+      </a>
+      <a
+        href="https://linkedin.com/in/julian-hernandez-1a0237252"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={linkedinLogo} />
+      </a>
+    </footer>
   );
 }
