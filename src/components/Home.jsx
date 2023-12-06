a26a2821494f7dcaf26557d693597104abf2880c
julian-hzd <julianhzd@hotmail.com>
2023-12-05 21:59:50 -0500

    hover effects on name and passion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,22 @@ export default function Home() {
   const linkText = "text-red-300 hover:text-red-400";
   return (
     <div className="flex flex-wrap justify-center">
-      <div className="w-3/6 ">
+      <div className="w-3/6 drop-shadow-2xl">
         <h1 className="mb-8 text-6xl text-left text-gray-400">
           <Link to={`/home`} className="hover:text-gray-300">
             Welcome!
           </Link>
         </h1>
         <p className="text-2xl leading-loose">
-          I'm Julian Hernandez a Computer Science graduate passionate about
-          Software Development, seeking adventures. Feel free to check out the
+          I'm{" "}
+          <span className="text-purple-100 hover:text-purple-300">
+            Julian Hernandez
+          </span>{" "}
+          a Computer Science graduate passionate about{" "}
+          <span className="text-gray-400 hover:text-gray-600">
+            Software Development
+          </span>
+          , seeking adventures. Feel free to check out the
           <Link to={`/projects`} className={`${linkText}`}>
             {" "}
             projects{" "}
