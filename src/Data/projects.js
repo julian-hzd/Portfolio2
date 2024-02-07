6a99d5afdfc8b4c50b6b069602e5cfdda51b68b6
julian-hzd <julianhzd@hotmail.com>
2024-02-06 21:31:34 -0500

    Exported projects to its own file, rendering all projects based on the array

---
+++ src/Data/projects.js
@@ -0,0 +1,9 @@
+const projects = [
+  {
+    title: "The Legend",
+    description: "Platformer Game",
+    image: "/game-ss.png",
+    languages: "/js.svg",
+  },
+];
+export default projects;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,6 @@
-import game from "/game-ss.png";
-import js from "/js.svg";
 import { useState } from "react";
 import ShowProject from "./ShowProject";
+import projects from "../Data/projects.js";
 
 export default function Projects() {
   const [displayImg, setDisplayImg] = useState();
@@ -11,32 +10,40 @@ export default function Projects() {
   };
   return (
     <>
-      {displayImg && <ShowProject image={game} CloseImage={CloseImage} />}
-      <div className="flex flex-wrap justify-center w-full space-x-20 ">
-        <div
-          className="border-double border-4 border-zinc-500 p-7 hover:bg-gray-800 cursor-pointer"
-          id="game-ss.png"
-          onClick={() => {
-            setDisplayImg(game);
-          }}
-        >
-          <div className="">
-            <a
-              href="https://github.com/julian-hzd/the-legend-game"
-              target="_blank"
-              rel="noreferrer"
-              className="text-2xl text-yellow-100 "
+      {displayImg && <ShowProject image={displayImg} CloseImage={CloseImage} />}
+      {projects.map((project) => {
+        return (
+          <div
+            className="flex flex-wrap justify-center w-full space-x-20 "
+            key={project.title}
+          >
+            <div
+              className="border-double border-4 border-zinc-500 p-7 hover:bg-gray-800 cursor-pointer"
+              onClick={() => {
+                setDisplayImg(project.image);
+              }}
             >
-              The Legend
-            </a>
-            <p className="italic mt-4 text-zinc-300">Platformer game</p>
+              <div className="">
+                <a
+                  href="https://github.com/julian-hzd/the-legend-game"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-2xl text-yellow-100 "
+                >
+                  {project.title}
+                </a>
+                <p className="italic mt-4 text-zinc-300">
+                  {project.description}
+                </p>
+              </div>
+              <div className="flex flex-wrap justify-center items-center space-x-5">
+                <button className="border-2 my-4 bg-zinc-700">&lt; &gt;</button>
+                <img src={project.languages}></img>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-wrap justify-center items-center space-x-5">
-            <button className="border-2 my-4 bg-zinc-700">&lt; &gt;</button>
-            <img src={js}></img>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </>
   );
 }
